9e5c0781bcf11268d7f17077f720c52965fbf442
Lalida Tansuvanwong <lalida.t@MBKL000490.local>
2023-01-17 22:28:59 +0700

    Publish Web Playground with Github Pages

--- api-assump-chan/src/configs/Firestore.js
+++
@@ -1,5 +0,0 @@
-const Firestore = require('@google-cloud/firestore');
-
-const { firestore: { projectId, keyFilename } } = require('./')
-
-module.exports = new Firestore({ projectId, keyFilename });
--- api-assump-chan/src/models/index.js
+++
@@ -1,8 +0,0 @@
-const FirestoreDB = require('../configs/Firestore');
-const UserModel = require('./UserModel')
-const TimeModel = require('./TimeModel')
-
-module.exports = {
-  UserModel: new UserModel(FirestoreDB),
-  TimeModel: new TimeModel(FirestoreDB),
-};
